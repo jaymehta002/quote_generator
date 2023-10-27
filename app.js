060ca9ef306c2252149d40187d69029ea32b6aea
jaymehta002 <jaymehta002@gmail.com>
2023-10-27 11:00:59 +0530

    updated

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const express = require("express");
-const { getQuote } = require("node-quotegen");
+// app.js
+const express = require('express');
+const { getQuote } = require('node-quotegen');
+const path = require('path');
 
 const app = express();
 
-app.get("/api/quote", (req, res) => {
-  const quote = getQuote();
-  res.json({ quote });
-});
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.redirect('/index.html');
-});
-app.get("/api/quote/:category", (req, res) => {
-  const quote = getQuote(req.params.category);
-  res.json({ quote });
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.get('/api/quote', (req, res) => {
+    const quote = getQuote();
+    res.json({ quote });
+});
+
+app.get('/api/quote/:category', (req, res) => {
+    const quote = getQuote(req.params.category);
+    res.json({ quote });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-
-
 const quoteText = document.getElementById('quote-output');
 const generateBtn = document.getElementById('generate-button');
 const categorySelect = document.getElementById('quote-type');
@@ -8,13 +6,11 @@ const copyBtn = document.getElementById('copy-button');
 generateBtn.addEventListener('click', () => {
     const category = categorySelect.value;
     if (category !== 'none') {
-        const url = `/api/quote/${category}`;
-        console.log(url);
+        const url = `api/quote/${category}`;
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 quoteText.value = data.quote;
-                console.log(url);
             })
             .catch(error => {
                 console.error('Error fetching quote:', error);
@@ -25,14 +21,12 @@ generateBtn.addEventListener('click', () => {
             .then(response => response.json())
             .then(data => {
                 quoteText.value = data.quote;
-                console.log(url);
             })
             .catch(error => {
                 console.error('Error fetching quote:', error);
             });
     }
 });
-
 
 copyBtn.addEventListener('click', () => {
     quoteText.select();
