22c61ebd7422c029337bcc3f4f207feda4312b3b
Jay Mehta <40739838+jaymehta002@users.noreply.github.com>
2023-10-27 12:05:33 +0530

    updated ui/ux

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ const quoteText = document.getElementById('quote-output');
 const generateBtn = document.getElementById('generate-button');
 const categorySelect = document.getElementById('quote-type');
 const copyBtn = document.getElementById('copy-button');
+const copySuccessAlert = document.querySelector('.copy-success-alert');
 
 generateBtn.addEventListener('click', () => {
     const category = categorySelect.value;
@@ -31,9 +32,25 @@ generateBtn.addEventListener('click', () => {
 copyBtn.addEventListener('click', () => {
     quoteText.select();
     document.execCommand('copy');
-    alert('Copied to clipboard');
+    copySuccessAlert.style.display = 'block';
+    setTimeout(() => {
+        copySuccessAlert.style.display = 'none';
+    }, 1000);
 });
 
 
+const popupDialog = document.getElementById('popup-dialog');
 
+copyBtn.addEventListener('click', () => {
+    const quoteText = document.getElementById('quote-output');
+    quoteText.select();
+    document.execCommand('copy');
 
+    // Show the pop-up dialog
+    popupDialog.style.display = 'block';
+
+    // Hide the pop-up dialog after 2 seconds (2000 milliseconds)
+    setTimeout(() => {
+        popupDialog.style.display = 'none';
+    },1000);
+});
